9264eb58b481cec3802d7d896afcc6afb7708d63
YJ <soltwood6862@naver.com>
2022-11-04 17:50:35 +0900

    [feat] findUserAll 기능 구현

--- java/src/main/java/com/hello/java/domain/board/controller/UserController.java
+++ java/src/main/java/com/hello/java/domain/board/controller/UserController.java
@@ -2,7 +2,9 @@ package com.hello.java.domain.board.controller;
 
 import com.hello.java.domain.board.User;
 import com.hello.java.domain.board.UserService;
+import com.hello.java.domain.board.dto.UserListResponseDto;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,11 @@ public class UserController {
     @PostMapping("/user")
     public boolean addUser(@RequestParam("userId") String userId, @RequestParam("password") String password) {
         return userService.addUser(userId, password);
+    }
+
+    @GetMapping("/user")
+    public UserListResponseDto findUserAll() {
+        return userService.findUserAll();
     }
 
 }
