6b2bec9a0020ba5e21034a07d0acf3ac61802727
YJ <soltwood6862@naver.com>
2022-11-04 18:17:41 +0900

    [fix] id에 @GeneratedValue 붙여줌

--- java/src/main/java/com/hello/java/domain/board/User.java
+++ java/src/main/java/com/hello/java/domain/board/User.java
@@ -6,13 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String password;
