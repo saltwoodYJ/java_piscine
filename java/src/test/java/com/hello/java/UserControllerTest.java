e60f40e4d2981d90b7cd31651c27e99b85b54503
saltwoodYJ <soltwood6862@naver.com>
2022-11-07 14:33:48 +0900

    [feat] 유저찾기 테스트코드 추가

--- java/src/test/java/com/hello/java/UserControllerTest.java
+++ java/src/test/java/com/hello/java/UserControllerTest.java
@@ -1,2 +1,41 @@
-package com.hello.java;public class UserControllerTest {
+package com.hello.java;
+
+import com.hello.java.domain.board.User;
+import com.hello.java.domain.board.UserRepository;
+import com.hello.java.domain.board.UserService;
+import com.hello.java.domain.board.dto.UserListResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void 유저찾기(){
+        //given
+        String name = "name";
+        String password = "password";
+        User userAdd = User.builder()
+                .userName(name)
+                .password(password)
+                .build();
+        userService.addUser(name, password);
+
+        //when
+        User getUser = userService.findUserOne("name");
+
+        //then
+        Assertions.assertThat(name).isEqualTo(getUser.getUserName());
+        Assertions.assertThat(password).isEqualTo(getUser.getPassword());
+
+    }
+
+
 }
